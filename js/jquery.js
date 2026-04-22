802b561fc11f46457da9ce3c9aaf90d95da6ea23
Ramprasad Karak <ramprasadkarak321@gmail.com>
2026-04-23 01:13:06 +0530

    last commit

--- js/jquery.js
+++ js/jquery.js
@@ -1,25 +1,7 @@
-// $(function () {
-//   let stickyNavTp = $(".header-btm").offset().top;
-//   function styckyView() {
-//     let scrollTp = $(window).scrollTop();
-//     if (scrollTp > stickyNavTp) {
-//       $(".header-btm").addClass("nav-sticky");
-//     } else {
-//       $(".header-btm").removeClass("nav-sticky");
-//     }
-//   }
-//   styckyView();
-
-//   $(window).scroll(function () {
-//     styckyView();
-//   });
-// });
-
-
 $(function () {
   let stickyNavTp = $(".header-btm").offset().top;
 
-  // Lenis ke scroll event use karo (jQuery ki jagah)
+  // Lenis ke scroll event use karo
   lenis.on('scroll', ({ scroll }) => {
     if (scroll > stickyNavTp) {
       $(".header-btm").addClass("nav-sticky");
