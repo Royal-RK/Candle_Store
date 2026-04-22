b79b2b84d10885f526a22ea471f7248e0ca5eb38
Ramprasad Karak <ramprasadkarak321@gmail.com>
2026-04-22 23:09:33 +0530

    add some animation

--- js/jquery.js
+++ js/jquery.js
@@ -1,17 +1,30 @@
+// $(function () {
+//   let stickyNavTp = $(".header-btm").offset().top;
+//   function styckyView() {
+//     let scrollTp = $(window).scrollTop();
+//     if (scrollTp > stickyNavTp) {
+//       $(".header-btm").addClass("nav-sticky");
+//     } else {
+//       $(".header-btm").removeClass("nav-sticky");
+//     }
+//   }
+//   styckyView();
+
+//   $(window).scroll(function () {
+//     styckyView();
+//   });
+// });
+
+
 $(function () {
   let stickyNavTp = $(".header-btm").offset().top;
 
-  function styckyView() {
-    let scrollTp = $(window).scrollTop();
-    if (scrollTp > stickyNavTp) {
+  // Lenis ke scroll event use karo (jQuery ki jagah)
+  lenis.on('scroll', ({ scroll }) => {
+    if (scroll > stickyNavTp) {
       $(".header-btm").addClass("nav-sticky");
     } else {
       $(".header-btm").removeClass("nav-sticky");
     }
-  }
-  styckyView();
-
-  $(window).scroll(function () {
-    styckyView();
   });
 });
--- js/script.js
+++ js/script.js
@@ -1,50 +1,61 @@
+// ============ AOS INIT (sabse pehle) ============
+AOS.init({
+  duration: 800,
+  offset: 80,
+  once: true,
+  easing: "ease-in-out",
+});
+// ============ LENIS SMOOTH SCROLL ============
+const lenis = new Lenis({
+  duration: 1.2,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+  orientation: "vertical",
+  smoothWheel: true,
+});
+
+// Lenis + GSAP ScrollTrigger sync
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+requestAnimationFrame(raf);
+// Lenis + AOS sync
+lenis.on("scroll", () => {
+  AOS.refresh();
+});
+
+// ============ MOBILE MENU ============
 let menuBtn = document.querySelector(".menu-btn");
 let navMenu = document.querySelector(".nav-menu");
-
 let isOpen = false;
 
 menuBtn.addEventListener("click", () => {
   if (isOpen === false) {
-    navMenu.style.left = "-0%";
+    navMenu.style.left = "0%";
     document.body.classList.add("no-scroll");
+    lenis.stop();
     isOpen = true;
   } else {
     navMenu.style.left = "-100%";
     document.body.classList.remove("no-scroll");
+    lenis.start();
     isOpen = false;
   }
 });
-// window.addEventListener("scroll", () => {
-//   let btmHeader = document.querySelector(".header-btm");
-//   if (window.scrollY > 100) {
-//     btmHeader.classList.add("custom-sticky");
-//   } else {
-//     btmHeader.classList.remove("custom-sticky");
-//   }
-// });
-// Bnner slider
+
+// ============  SWIPER Js ============
 let bnrSwiper = new Swiper(".bnrSwiper", {
   loop: true,
-  // speed: 1000, 
   autoplay: {
-    delay: 3000, 
-    // disableOnInteraction: false,
+    delay: 3000,
   },
 });
-// Story slider
+
 let swiper = new Swiper(".strySwiper", {
   slidesPerView: 1,
   loop: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
-  },
-  breakpoints: {
-    768: {
-      slidesPerView: 1,
-    },
-    1024: {
-      slidesPerView: 1,
-    },
   },
 });
